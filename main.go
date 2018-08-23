3d22e4d47393b894fdb95cff4df08e92b7c61f07
Remi Ferrand <remi.ferrand@cc.in2p3.fr>
2018-08-23 17:21:20 +0200

    Do not rely on external *tar* command
    * Full go

--- main.go
+++ main.go
@@ -1,13 +1,109 @@
 package main
 
 import (
+	"archive/tar"
 	"crypto/subtle"
 	"io"
 	"log"
 	"net/http"
 	"os"
-	"os/exec"
+	"path/filepath"
 )
+
+type tarStreamer struct {
+	c    chan []byte
+	done chan struct{}
+}
+
+func newTarStreamer(dir string) tarStreamer {
+	return tarStreamer{
+		c:    make(chan []byte),
+		done: make(chan struct{}, 1),
+	}
+}
+
+func (ts tarStreamer) Done() {
+	ts.done <- struct{}{}
+}
+
+func (ts tarStreamer) Write(p []byte) (int, error) {
+	ts.c <- p
+	return len(p), nil
+}
+
+func (ts tarStreamer) Read(p []byte) (int, error) {
+	select {
+	case buf := <-ts.c:
+		return copy(p, buf), nil
+	case <-ts.done:
+		return 0, io.EOF
+	}
+}
+
+type writerDoner interface {
+	Write(p []byte) (int, error)
+	Done()
+}
+
+func buildTarArchive(baseDir string, wd writerDoner) {
+	tw := tar.NewWriter(wd)
+
+	err := filepath.Walk(baseDir, func(path string, info os.FileInfo, ferr error) error {
+
+		var hdr *tar.Header
+
+		//relativePath := strings.Replace(path, baseDir, "", 1)
+		// the tar command does not include leading "/"
+		// based on `tar cf - /tmp/dir | tar tvf -` output
+		relativePath := path[1:]
+
+		if info.IsDir() {
+			hdr = &tar.Header{
+				Name:     relativePath,
+				Mode:     0700,
+				ModTime:  info.ModTime(),
+				Typeflag: tar.TypeDir,
+			}
+		} else {
+			hdr = &tar.Header{
+				Name:     relativePath,
+				Mode:     0600,
+				Size:     info.Size(),
+				ModTime:  info.ModTime(),
+				Typeflag: tar.TypeReg,
+			}
+		}
+
+		if err := tw.WriteHeader(hdr); err != nil {
+			log.Fatal(err)
+		}
+
+		if !info.IsDir() {
+			f, err := os.Open(path)
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer f.Close()
+
+			_, err = io.Copy(tw, f)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+
+		if err := tw.Flush(); err != nil {
+			log.Fatal(err)
+		}
+
+		return nil
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	tw.Close()
+	wd.Done()
+}
 
 func handlerFactory(baseDir string, username string, password string) http.HandlerFunc {
 
@@ -22,25 +118,17 @@ func handlerFactory(baseDir string, username string, password string) http.Handl
 			return
 		}
 
-		tar_cmd := exec.Command("tar", "cf", "-", baseDir)
+		// Tar pre
+		tstreamer := newTarStreamer("useless")
 
-		tar, err := tar_cmd.StdoutPipe()
-		if err != nil {
-			panic(err)
-		}
-		defer tar.Close()
-
-		if err := tar_cmd.Start(); err != nil {
-			panic(err)
-		}
-		defer tar_cmd.Wait()
+		go buildTarArchive(baseDir, tstreamer)
 
 		w.Header().Set("Content-Disposition", "attachment; filename=archive.tar")
 		w.Header().Set("Content-Type", "application/x-tar")
 
 		w.WriteHeader(http.StatusOK)
 
-		io.Copy(w, tar)
+		io.Copy(w, tstreamer)
 	}
 }
 
